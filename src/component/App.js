68d737c0e66e9fb15c99742cbd5fc514897bc95a
Ethan Titler <Titler.ethan.chs@gmail.com>
2018-09-26 16:21:19 -0500

    set up a subscribe in store to help visual testing

--- src/component/App.js
+++ src/component/App.js
@@ -9,23 +9,28 @@ import Dashboard from './Dashboard';
 const store = createAppStore();
 
 class App extends Component {
+  componentDidMount() {
+    store.subscribe(()=> {
+      console.log('sub says',store.getState());
+    });
+  }
   render() {
     return (
       <Provider store={store}>
-      <BrowserRouter>
-        <div className="App">
-          <header className="App-header">
-            <h1 className="App-title">KiloVolt-React</h1>
-            <h2>A Shocking way to display static objects</h2>
-            <nav>
-              <ul>
-                <li><Link to="/">Dashboard</Link></li>
-              </ul>
-            </nav>
-          </header>
-          <Route exact path="/" component={Dashboard}/>
-        </div>
-      </BrowserRouter>
+        <BrowserRouter>
+          <div className="App">
+            <header className="App-header">
+              <h1 className="App-title">KiloVolt-React</h1>
+              <h2>A Shocking way to display static objects</h2>
+              <nav>
+                <ul>
+                  <li><Link to="/">Dashboard</Link></li>
+                </ul>
+              </nav>
+            </header>
+            <Route exact path="/" component={Dashboard}/>
+          </div>
+        </BrowserRouter>  
       </Provider>
     );
   }
