86b032b91b53902d0c3e42f9ab58050b744ebba6
Ethan Titler <Titler.ethan.chs@gmail.com>
2018-09-27 18:13:57 -0500

    added updateGenre for reducer

--- src/reducer/genre-reducer.js
+++ src/reducer/genre-reducer.js
@@ -3,7 +3,9 @@ export default (state = [], action = {}) => {
     case 'GENRE_CREATE':
       return [ ...state, action.payload ];
     case 'GENRE_DELETE':
-      return state.filter(genre => genre._id !== action.payload._id)
+      return state.filter(genre => genre._id !== action.payload._id);
+    case 'GENRE_UPDATE':
+      return state.map(genre=> genre._id === action.payload._id ? action.payload : genre);
     default:
       return state;
   }
