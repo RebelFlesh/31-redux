51777e4c79523dcde1217377e5f8974e4ede5ea1
Ethan Titler <Titler.ethan.chs@gmail.com>
2018-09-28 10:01:38 -0500

    form optimized for both update and create

--- src/component/Form.js
+++ src/component/Form.js
@@ -5,18 +5,23 @@ export default class Form extends Component {
   handleSubmit = (event) => {
     event.preventDefault();
     let book={};
+    if(this.props.currentGenre){
+      book._id = this.props.currentGenre._id;
+    }
     book.title=event.target.title.value;
     book.author=event.target.author.value;
     this.props.handleComplete(book);
   }
 
   render(){
+    let buttonText = this.props.currentGenre ? 'update' : 'submit';
+
     return(
       <React.Fragment>
         <form onSubmit={this.handleSubmit}>
           <input type="text" name="title" placeholder="title"></input>
           <input type="text" name="author" placeholder="author"></input>
-          <input type="submit"></input>
+          <input type="submit" value={buttonText}></input>
         </form>
       </React.Fragment>
     )
